05c293a7d6ee1e8d333824befb257a9afb5e6949
Jenna Kovacs <jkovacs@ufl.edu>
2017-10-03 11:44:35 -0400

    started home page - added a jumbotron element and image for sign in/up buttons

--- src/header/Header.js
+++ src/header/Header.js
@@ -17,7 +17,7 @@ class Header extends React.Component {
             <Navbar collapseOnSelect fixedTop className="test">
                 <Navbar.Header>
                     <Navbar.Brand>
-                        Recipe Sharing Site
+                        Reciprocity
                     </Navbar.Brand>
                     <Navbar.Toggle />
                 </Navbar.Header>
